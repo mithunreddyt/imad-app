5245db98d21ae134c3ae9d71b5e2fc09ee30861c
Mithun reddy <mithunreddythunga@gmail.com>
2017-08-05 18:30:59 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req, res){
-    res.sendFile('Article one is requested and served here');
+    res.send('Article one is requested and served here');
 });
 
 app.get('/article-two', function(req, res){
-    res.sendFile('Aticle two is requested and served here');
+    res.send('Aticle two is requested and served here');
 });
 
 app.get('/article-three', function(req, res){
-    res.sendFile('Article three is required and served here');
+    res.send('Article three is required and served here');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
