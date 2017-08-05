70e4a07a46b676d3a2c93455b429455cf5765358
Mithun reddy <mithunreddythunga@gmail.com>
2017-08-05 18:29:13 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function(req, res){
+    res.senFile('Article one is requested and served here');
+});
+
+app.get('/article-two', function(req, res){
+    res.sendFile('Aticle two is requested and served here');
+});
+
+app.get('/article-three', function(req, res){
+    res.sendFile('Article three is required and served here');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
